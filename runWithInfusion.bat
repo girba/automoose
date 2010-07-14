d96b9a2bb8d67de91580d2945a3a8be904deb67d
Tudor Girba <tudor.girba@gmail.com>
2010-07-14 12:10:10 +0200

    added runWithInfusion.bat

---
+++ runWithInfusion.bat
@@ -0,0 +1,14 @@
+echo ------ AUTO MOOSE STARTED ------
+
+set ROOT=%CD%
+
+set BUILD_PATH=%WORKSPACE%
+set PHARO_VM="%ROOT%\pharo\PharoVM-Win32-4\Pharo.exe"
+
+SRC_PATH="%ROOT%\src"
+
+SCRIPTS_PATH="%ROOT%\scripts"
+
+echo %BUILD_PATH%
+echo %ROOT%
+echo %PHARO_VM%
