9cd40e43f052a22016c099344f0772afcc290170
Tudor Girba <tudor.girba@gmail.com>
2010-07-14 14:11:09 +0200

    try

--- runWithInfusion.bat
+++ runWithInfusion.bat
@@ -17,9 +17,10 @@ set MOOSE_FILE="moose-"
 set MOOSE_IMAGE_FILE="%MOOSE_FILE%.image"
 set MOOSE_CHANGES_FILE="%MOOSE_FILE%.changes"
 
-md %BUILD_PATH%/%MOOSE_FILE%
-cd %BUILD_PATH%/%MOOSE_FILE%
+md "%BUILD_PATH%\%MOOSE_FILE%"
+cd "%BUILD_PATH%\%MOOSE_FILE%"
 
+echo FOR /F "TOKENS=1* DELIMS= " %%A IN ('TIME/T') DO SET CTIME=%%B For /f "tokens=1-4 delims=: " %%a in ('time /t') do (set time=%%a%%b%%c)
 
 echo %BUILD_PATH%
 echo %ROOT%
