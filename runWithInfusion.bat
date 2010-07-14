df835c0b41bfcd8226963d0becbf3bced10ccedc
Tudor Girba <tudor.girba@gmail.com>
2010-07-14 12:22:58 +0200

    added paths

--- runWithInfusion.bat
+++ runWithInfusion.bat
@@ -4,11 +4,22 @@ echo ------ AUTO MOOSE STARTED ------
 set ROOT=%CD%
 
 set BUILD_PATH=%WORKSPACE%
-set PHARO_VM="%ROOT%\pharo\PharoVM-Win32-4\Pharo.exe"
+set BUILD_PATH=%ROOT%\build
+set PHARO_VM=%ROOT%\pharo\PharoVM-Win32-4\Pharo.exe
 
-SRC_PATH="%ROOT%\src"
+set SRC_PATH="%ROOT%\src"
 
-SCRIPTS_PATH="%ROOT%\scripts"
+set SCRIPTS_PATH="%ROOT%\scripts"
+
+set COMPLETE_SCRIPT="$SCRIPTS_PATH/to-run.st"
+
+set MOOSE_FILE="moose-"
+set MOOSE_IMAGE_FILE="%MOOSE_FILE%.image"
+set MOOSE_CHANGES_FILE="%MOOSE_FILE%.changes"
+
+md %BUILD_PATH%/%MOOSE_FILE%
+cd %BUILD_PATH%/%MOOSE_FILE%
+
 
 echo %BUILD_PATH%
 echo %ROOT%
